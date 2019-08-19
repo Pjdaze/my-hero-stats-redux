b49c7dd14039822c3de360024caf5956eec798ee
Pjdaze <pjdazeux@gmail.com>
2019-08-19 07:00:49 -0400

    stable

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,7 +18,7 @@ class Home extends React.Component {
           heroes: [...data]
         });
       })
-      .catch(err => console.log(err));
+      .catch(err => console.log("Here is Your Error", err));
   }
 
   render() {
@@ -28,20 +28,24 @@ class Home extends React.Component {
       x => x.name.slice(0, 3).toLowerCase() === "bat"
     );
 
-    const getBatman = getBats.filter(hero => hero.slug === "69-batman");
+    const getBatman = getBats.filter(hero => hero.slug === "70-batman");
     const batName = getBatman.map(x => x.name);
+    const getPowers = getBats.map(x => x.powerstats.spped);
 
     console.log("These are the Bats ", ...getBats);
-    console.log("This is Batman's profile", getBatman.map(x => x.images));
+    console.log("This is Batman's profile", getBatman);
+    console.log();
+
     return (
       <HomeWrap>
-        <div className="hero-box" heroes={heroes}>
+        <div className="hero-box">
           <p>Name: {batName}</p>
           <img
             style={{ height: "200px", width: "200px" }}
             src={getBatman.map(x => x.images.md)}
             alt="x"
           />
+          <h4>Habilities: {getPowers} </h4>
         </div>
         <button onClick={this.getBats}>Get Random Hero</button>
       </HomeWrap>
