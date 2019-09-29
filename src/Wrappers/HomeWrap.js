051270790519f1df4e6540679c4ceb2c8d1e4463
Pjdaze <pjdazeux@gmail.com>
2019-09-29 12:54:41 -0400

    stable

--- src/Wrappers/HomeWrap.js
+++ src/Wrappers/HomeWrap.js
@@ -3,15 +3,15 @@ import SpaceBG from "../assets/space.jpg";
 const HomeWrap = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+
   align-items: center;
   text-align: center;
   margin: 0 auto;
-
+  height: 100%;
   background-image: url(${SpaceBG});
   background-size: cover;
   background-repeat: no-repeat;
-  height: 100%;
+  min-height: 100vh;
 
   header {
     h1 {
